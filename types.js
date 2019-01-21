002487210f2a646cb453eb4fb3fb5d094886d129
BenasPelakauskas <g00356158@gmit.ie>
2019-01-21 17:18:10 +0000

    Exercise 4

---
+++ types.js
@@ -0,0 +1,16 @@
+// boolean
+var flag = true;
+console.log("Value assigned to flag is: " + flag);
+// number
+var decimal = 6;
+console.log("Value assigned to number is: " + decimal);
+// string
+var colour = "blue";
+console.log("Colour assigned to string is: " + colour);
+// array
+var array = [1, 2, 3];
+console.log("Numbers in array are: " + array);
+// any
+var notSure = 4;
+notSure = "Maybe a string instead?";
+notSure = false; // okay, definitely a boolean
