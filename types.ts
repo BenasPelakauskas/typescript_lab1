c1f6473bd64881c5c007d80faeb46863a476715a
BenasPelakauskas <g00356158@gmit.ie>
2019-01-21 17:15:18 +0000

    Exercise 4

---
+++ types.ts
@@ -0,0 +1,20 @@
+// boolean
+let flag: boolean = true;
+console.log("Value assigned to flag is: "+flag);
+
+// number
+let decimal: number = 6;
+console.log("Value assigned to number is: "+decimal);
+
+// string
+let colour: string = "blue";
+console.log("Colour assigned to string is: "+colour);
+
+// array
+let array: number[] = [1, 2, 3];
+console.log("Numbers in array are: "+array);
+
+// any
+let notSure: any = 4;
+notSure = "Maybe a string instead?";
+notSure = false; // okay, definitely a boolean
